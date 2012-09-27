66d6f07b5a536e2a64094f461bd8ecc71c75da80
zeratul021 <zeratul021@gmail.com>
2012-09-27 19:04:02 +0200

    Manual merge from https://issues.apache.org/jira/browse/WICKET-4019 version 1.5.7.2 Some cleanup ResponseState needs some decoupling and sanitation

--- jdk-1.6-parent/portlet-parent/wicketstuff-portlet/src/main/java/org/apache/wicket/portlet/EmbeddedPortletHeaderResponse.java
+++ jdk-1.6-parent/portlet-parent/wicketstuff-portlet/src/main/java/org/apache/wicket/portlet/EmbeddedPortletHeaderResponse.java
@@ -16,9 +16,6 @@
  */
 package org.apache.wicket.portlet;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.apache.wicket.markup.head.CssUrlReferenceHeaderItem;
 import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.markup.head.internal.HeaderResponse;
@@ -27,112 +24,115 @@ import org.apache.wicket.response.StringResponse;
 
 /**
  * Portlet behaviour override of the {@link HeaderResponse} implementation, responsible for writing
- * header contributions from portlets in the body of the response, as opposed to the head.
- * 
- * @see HeaderResponse
+ * header contributions from portlets to the body of the response, as opposed to the head.
+ *
  * @author Ate Douma
+ * @see HeaderResponse
  */
-public class EmbeddedPortletHeaderResponse extends HeaderResponse
-{
-	private final Response realResponse;
-	private final StringResponse bufferedResponse;
+public class EmbeddedPortletHeaderResponse extends HeaderResponse {
+    private final Response realResponse;
+    private final StringResponse bufferedResponse;
 
-	public EmbeddedPortletHeaderResponse(Response realResponse)
-	{
-		this.realResponse = realResponse;
-		bufferedResponse = new StringResponse();
-	}
+    public EmbeddedPortletHeaderResponse(Response realResponse) {
+        this.realResponse = realResponse;
+        this.bufferedResponse = new StringResponse();
+    }
 
-	@Override
-	public void render(HeaderItem headerItem)
-	{
-		if (!isClosed())
-		{
-			if (headerItem instanceof CssUrlReferenceHeaderItem)
-			{
-				CssUrlReferenceHeaderItem cssUrlReferenceHeaderItem = (CssUrlReferenceHeaderItem) headerItem;
-				String media = cssUrlReferenceHeaderItem.getMedia();
-				String url = cssUrlReferenceHeaderItem.getUrl();
+    @Override
+    public void render(HeaderItem headerItem) {
+        if (!isClosed()) {
+            if (headerItem instanceof CssUrlReferenceHeaderItem) {
+                CssUrlReferenceHeaderItem cssUrlReferenceHeaderItem = (CssUrlReferenceHeaderItem) headerItem;
+                String media = cssUrlReferenceHeaderItem.getMedia();
+                String url = cssUrlReferenceHeaderItem.getUrl();
 
-				List<?> token = Arrays.asList(new Object[] { "css", url, media });
+                CssRenderToken token = new CssRenderToken(url, media);
+                if (!wasRendered(token)) {
+                    // The CSS reference has not been written to the response yet
+                    getResponse().write("<script type=\"text/javascript\">");
+                    getResponse().write("var elem=document.createElement(\"link\");");
+                    getResponse().write("elem.setAttribute(\"rel\",\"stylesheet\");");
+                    getResponse().write("elem.setAttribute(\"type\",\"text/css\");");
+                    getResponse().write("elem.setAttribute(\"href\",\"" + url + "\");");
+                    if (media != null) {
+                        getResponse().write("elem.setAttribute(\"media\",\"" + media + "\");");
+                    }
+                    getResponse().write("document.getElementsByTagName(\"head\")[0].appendChild(elem);");
+                    getResponse().write("</script>");
+                    getResponse().write("\n");
+                    markRendered(token);
+                }
+            } else {
+                super.render(headerItem);
+            }
+        }
+    }
 
-				if (wasRendered(token) == false)
-				// The CSS reference has not been written to the response yet
-				{
-					getResponse().write("<script type=\"text/javascript\">");
-					getResponse().write("var elem=document.createElement(\"link\");");
-					getResponse().write("elem.setAttribute(\"rel\",\"stylesheet\");");
-					getResponse().write("elem.setAttribute(\"type\",\"text/css\");");
-					getResponse().write("elem.setAttribute(\"href\",\"" + url + "\");");
-					if (media != null)
-					{
-						getResponse().write("elem.setAttribute(\"media\",\"" + media + "\");");
-					}
-					getResponse().write("document.getElementsByTagName(\"head\")[0].appendChild(elem);");
-					getResponse().write("</script>");
-					getResponse().write("\n");
-					markRendered(token);
-				}
-			}
-			else
-			{
-				super.render(headerItem);
-			}
-		}
-	}
+    @Override
+    public void close() {
+        super.close();
+        // Automatically add <head> if necessary
+        CharSequence output = bufferedResponse.getBuffer();
+        if (output.length() > 0) {
+            if (output.charAt(0) == '\r') {
+                for (int i = 2; i < output.length(); i += 2) {
+                    char ch = output.charAt(i);
+                    if (ch != '\r') {
+                        output = output.subSequence(i - 2, output.length());
+                        break;
+                    }
+                }
+            } else if (output.charAt(0) == '\n') {
+                for (int i = 1; i < output.length(); i++) {
+                    char ch = output.charAt(i);
+                    if (ch != '\n') {
+                        output = output.subSequence(i - 1, output.length());
+                        break;
+                    }
+                }
+            }
+        }
+        if (output.length() > 0) {
+            realResponse.write("<span id=\"" + ThreadPortletContext.getNamespace() +
+                    "_embedded_head\" style=\"display:none\">");
+            realResponse.write(output);
+            realResponse.write("</span>");
+        }
+        bufferedResponse.reset();
+    }
 
-	/*
-	 * @see org.apache.wicket.markup.html.internal.HeaderResponse#close()
-	 */
-	@Override
-	public void close()
-	{
-		super.close();
-		// Automatically add <head> if necessary
-		CharSequence output = bufferedResponse.getBuffer();
-		if (output.length() > 0)
-		{
-			if (output.charAt(0) == '\r')
-			{
-				for (int i = 2; i < output.length(); i += 2)
-				{
-					char ch = output.charAt(i);
-					if (ch != '\r')
-					{
-						output = output.subSequence(i - 2, output.length());
-						break;
-					}
-				}
-			}
-			else if (output.charAt(0) == '\n')
-			{
-				for (int i = 1; i < output.length(); i++)
-				{
-					char ch = output.charAt(i);
-					if (ch != '\n')
-					{
-						output = output.subSequence(i - 1, output.length());
-						break;
-					}
-				}
-			}
-		}
-		if (output.length() > 0)
-		{
-			realResponse.write("<span id=\"" + ThreadPortletContext.getNamespace() +
-				"_embedded_head\" style=\"display:none\">");
-			realResponse.write(output);
-			realResponse.write("</span>");
-		}
-		bufferedResponse.reset();
-	}
+    @Override
+    protected Response getRealResponse() {
+        return bufferedResponse;
+    }
 
-	/*
-	 * @see org.apache.wicket.markup.html.internal.HeaderResponse#getRealResponse()
-	 */
-	@Override
-	protected Response getRealResponse()
-	{
-		return bufferedResponse;
-	}
+    private static class CssRenderToken {
+        private final String url;
+        private final String media;
+
+        private CssRenderToken(String url, String media) {
+            if (url == null) {
+                throw new IllegalArgumentException("Url can't be null.");
+            }
+            this.url = url;
+            this.media = media;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            CssRenderToken that = (CssRenderToken) o;
+            return url.equals(that.url) && (media != null ? media.equals(that.media) : that.media == null);
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * url.hashCode() + (media != null ? media.hashCode() : 0);
+        }
+    }
 }
--- jdk-1.6-parent/portlet-parent/wicketstuff-portlet/src/main/java/org/apache/wicket/portlet/Utils.java
+++ jdk-1.6-parent/portlet-parent/wicketstuff-portlet/src/main/java/org/apache/wicket/portlet/Utils.java
@@ -16,86 +16,91 @@
  */
 package org.apache.wicket.portlet;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.wicket.util.encoding.UrlDecoder;
 import org.apache.wicket.util.encoding.UrlEncoder;
 import org.apache.wicket.util.string.Strings;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 100 times already implemented functions (partly taken from Wicket Url)
- * 
+ *
  * @author Peter Pastrnak
  */
 public final class Utils {
-	private static final String DEFAULT_CHARSET_NAME = "UTF-8";
+    private static final String DEFAULT_CHARSET_NAME = "UTF-8";
 
-	public static final boolean appendParameter(StringBuilder sb, boolean first, String name, String value) {
-		if (!first) {
-			sb.append('&');
-		}
-		else {
-			first = false;
-		}
+    public static final boolean appendParameter(StringBuilder sb, boolean first, String name, String value, boolean encodeURL) {
+        if (!first) {
+            sb.append('&');
+        } else {
+            first = false;
+        }
 
-		sb.append(UrlEncoder.QUERY_INSTANCE.encode(name, DEFAULT_CHARSET_NAME));
-		if (value != null) {
-			sb.append('=').append(UrlEncoder.QUERY_INSTANCE.encode(value, DEFAULT_CHARSET_NAME));
-		}
-		return first;
-	}
+        if (encodeURL) {
+            sb.append(UrlEncoder.QUERY_INSTANCE.encode(name, DEFAULT_CHARSET_NAME));
+            if (value != null) {
+                sb.append('=').append(UrlEncoder.QUERY_INSTANCE.encode(value, DEFAULT_CHARSET_NAME));
+            }
+        } else {
+            sb.append(name);
+            if (value != null) {
+                sb.append('=').append(value);
+            }
+        }
+        return first;
+    }
 
-	public static final String buildQueryString(Map<String, String[]> parameterMap) {
-		if ((parameterMap == null) || (parameterMap.isEmpty())) {
-			return null;
-		}
+    public static final String buildQueryString(Map<String, String[]> parameterMap) {
+        return buildQueryString(parameterMap, true);
+    }
 
-		boolean first = true;
-		StringBuilder sb = new StringBuilder();
+    public static final String buildQueryString(Map<String, String[]> parameterMap, boolean encodeURL) {
+        if ((parameterMap == null) || (parameterMap.isEmpty())) {
+            return null;
+        }
 
-		for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-			String name = entry.getKey();
-			if ((!name.startsWith("p_p_")) || (!WicketPortlet.WICKET_URL_PORTLET_PARAMETER.equals(name))) {
+        boolean first = true;
+        StringBuilder sb = new StringBuilder();
 
-				String[] values = entry.getValue();
-				if ((values == null) || (values.length == 0)) {
-					first = appendParameter(sb, first, name, null);
-				}
-				else {
-					for (String value : values) {
-						first = appendParameter(sb, first, name, value);
-					}
-				}
-			}
-		}
-		return sb.toString();
-	}
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+            String name = entry.getKey();
+            String[] values = entry.getValue();
+            if ((values == null) || (values.length == 0)) {
+                first = appendParameter(sb, first, name, null, encodeURL);
+            } else {
+                for (String value : values) {
+                    first = appendParameter(sb, first, name, value, encodeURL);
+                }
+            }
+        }
+        return sb.toString();
+    }
 
-	public static final Map<String, String[]> parseQueryString(String queryString) {
-		Map<String, String[]> parameterMap = new HashMap<String, String[]>();
-		if (queryString != null) {
-			for (String queryParameter : Strings.split(queryString, '&')) {
+    public static final Map<String, String[]> parseQueryString(String queryString) {
+        Map<String, String[]> parameterMap = new HashMap<String, String[]>();
+        if (queryString != null) {
+            for (String queryParameter : Strings.split(queryString, '&')) {
 
-				String parts[] = Strings.split(queryParameter, '=');
-				if (parts.length > 0) {
-					for (int i = 0; (i < 2) && (i < parts.length); i++) {
-						parts[i] = UrlDecoder.QUERY_INSTANCE.decode(parts[i], DEFAULT_CHARSET_NAME);
-					}
+                String parts[] = Strings.split(queryParameter, '=');
+                if (parts.length > 0) {
+                    for (int i = 0; (i < 2) && (i < parts.length); i++) {
+                        parts[i] = UrlDecoder.QUERY_INSTANCE.decode(parts[i], DEFAULT_CHARSET_NAME);
+                    }
 
-					String[] values = parameterMap.get(parts[0]);
-					if (values == null) {
-						parameterMap.put(parts[0], new String[] { (parts.length > 1 ? parts[1] : null) });
-					}
-					else {
-						String[] moreValues = new String[values.length + 1]; 
-						System.arraycopy(values, 0, moreValues, 0, values.length);
-						moreValues[values.length - 1] = (parts.length > 1 ? parts[1] : null);
-						parameterMap.put(parts[0], moreValues);
-					}
-				}
-			}
-		}
-		return parameterMap;
-	}
+                    String[] values = parameterMap.get(parts[0]);
+                    if (values == null) {
+                        parameterMap.put(parts[0], new String[] { (parts.length > 1 ? parts[1] : null) });
+                    } else {
+                        values = Arrays.copyOf(values, values.length + 1);
+                        values[values.length - 1] = (parts.length > 1 ? parts[1] : null);
+                        parameterMap.put(parts[0], values);
+                    }
+                }
+            }
+        }
+        return parameterMap;
+    }
 }
